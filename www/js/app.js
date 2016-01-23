0db3d28bc113afcaac873e1d17d0be17cf9adf63
Adam Weitzman <adamcweitzman@gmail.com>
2016-01-23 17:48:26 -0600

    fixed routing

--- www/js/app.js
+++ www/js/app.js
@@ -68,7 +68,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/home');
+  // $urlRouterProvider.otherwise('/home');
 
   /*
   // setup an abstract state for the tabs directive
